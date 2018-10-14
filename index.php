9ee6ccafd2377d7362bc16dada3de81f1a98d0ef
Fabio Carneiro de Campos <43301886+fabiocarneirodecampos@users.noreply.github.com>
2018-10-14 17:12:51 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once("config.php");
 
-$root = new Usuario();
+//carrega um usuario
+//$root = new Usuario();
+//$root->loadbyId(3);
+//echo $root;
 
-$root->loadbyId(3);
+//carrega uma lista de usuários
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $root;
+//carrega uma lista de usuários buscando pelo login
+//$search = Usuario::search("jo");
+//echo json_encode($search);
 
+//carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("root","!@#$");
+
+echo $usuario;
  ?>
